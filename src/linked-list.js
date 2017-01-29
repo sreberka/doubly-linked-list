8759435f9a348c902986c085556b854531933de9
sreberka <vitalia.serebro@gmail.com>
2017-01-29 17:53:15 +0300

    some changes

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,25 +1,56 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.arr = [];
+        this.data = null;
+        this.length = this.arr.length;
+    }
 
-    append(data) {}
+    append(data) {
+        this.data = data;
+        this.arr.push(this.data);
+        this.length++;
+        let node = new Node(this.data, this._head, this._tail);
+        if(this.length === 0){
+            this._head = node.prev;
+            this._tail = node.next;
+        }
+    }
 
-    head() {}
+    head() {
+        return this.arr[0]
+    }
 
-    tail() {}
+    tail() {
+        return this.arr[this.arr.length - 1]
+    }
 
-    at(index) {}
+    at(index) {
 
-    insertAt(index, data) {}
+    }
 
-    isEmpty() {}
+    insertAt(index, data) {
+
+    }
+
+    isEmpty() {
+        if(this.arr.length === 0){
+            return true
+        }
+
+    }
 
     clear() {}
 
-    deleteAt(index) {}
+    deleteAt(index) {
+        this.index = index;
+        return this.arr.splice(this.index, 1)
+    }
 
-    reverse() {}
+    reverse() {
+        this.arr.reverse()
+    }
 
     indexOf(data) {}
 }
